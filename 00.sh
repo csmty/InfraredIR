060a3c2d6b0407f1af1429efbbb643887fbcce64
tyma <tyma0913@gmail.com>
2026-07-17 21:50:13 +0800

    Remove local helper script and segformer log from release

--- 00.sh
+++
@@ -1,6 +0,0 @@
-python tools/compare_weights.py \
-  ./experiments/stage1_single1/checkpoints/model_1001.pkl \
-  ./experiments/stage2_finetune/checkpoints/model_501.pkl \
-  --topk 50 \
-  --show-missing \
-  --show-shape-mismatch
